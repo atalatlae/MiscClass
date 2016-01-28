f1546409ff0352e2467211260772a4807111de67
Boris <atalatlae@gmail.com>
2016-01-28 14:03:53 -0300

    Added new class for sanitization

---
+++ Sanitize/Sanitize.php
@@ -0,0 +1,61 @@
+<?php
+
+define('FS_STRING', 1);
+define('FS_EMAIL', 2);
+define('FS_FLOAT', 3);
+define('FS_INT', 4);
+define('FS_URL', 5);
+define('FS_ARRAY', 6);
+define('FS_ARRAY_STRING', 7);
+define('FS_ARRAY_EMAIL', 8);
+define('FS_ARRAY_FLOAT', 9);
+define('FS_ARRAY_INT', 10);
+define('FS_ARRAY_URL', 11);
+
+class Sanitize
+{
+	public function sanitizeVar($var = null, $type = "")
+	{
+		switch($type) {
+			case FS_STRING:
+				return filter_var($var, FILTER_SANITIZE_STRING);
+				break;
+			case FS_EMAIL:
+				return filter_var($var, FILTER_SANITIZE_EMAIL);
+				break;
+			case FS_FLOAT:
+				return filter_var($var, FILTER_SANITIZE_NUMBER_FLOAT);
+				break;
+			case FS_INT:
+				return filter_var($var, FILTER_SANITIZE_NUMBER_INT);
+				break;
+			case FS_URL:
+				return filter_var($var, FILTER_SANITIZE_URL);
+				break;
+			case FS_ARRAY:
+				if (!is_array($var)) {
+					return array();
+				}
+				return $var;
+				break;
+			case FS_ARRAY_STRING:
+				return filter_var_array($var, FILTER_SANITIZE_STRING);
+				break;
+			case FS_ARRAY_EMAIL:
+				return filter_var_array($var, FILTER_SANITIZE_EMAIL);
+				break;
+			case FS_ARRAY_FLOAT:
+				return filter_var_array($var, FILTER_SANITIZE_NUMBER_FLOAT);
+				break;
+			case FS_ARRAY_INT:
+				return filter_var_array($var, FILTER_SANITIZE_NUMBER_INT);
+				break;
+			case FS_ARRAY_URL:
+				return filter_var_array($var, FILTER_SANITIZE_URL);
+				break;
+			default:
+				return $var;
+		}
+	}
+}
+
---
+++ Sanitize/example.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once('Sanitize.php');
+
+$snt = new Sanitize();
+
+$string = 'Hello <b>world</b>';
+$clean = $snt->sanitizeVar($string, FS_STRING);
+var_dump($clean);
+
+$email = '(foo@var.com)';
+$clean = $snt->sanitizeVar($email, FS_EMAIL);
+var_dump($clean);
+
+$emails = array(
+	'foo@var.com',
+	'(foo@var.com)',
+	'<foo@var.com>'
+);
+$clean = $snt->sanitizeVar($emails, FS_ARRAY_EMAIL);
+var_dump($clean);
+
+$noarray = 1;
+$clean = $snt->sanitizeVar($noarray, FS_ARRAY);
+var_dump($clean);
