8cba7d8c1b9f2f28ae36905c44609e2abe198834
Boris <atalatlae@gmail.com>
2016-06-10 08:25:45 -0400

    ISSUE #1: Some times the decrypt() method fail
    When parse the data, get this error message:
    Warning: mcrypt_generic_init(): Iv size incorrect; supplied length: 4, needed: 8
    
    [FIX] Now, when parsing the raw data to get the initiazation vector, use the
    ivSize not explit the data for a specific char. The error appear when the iv
    contains a character equal to field separator

--- Crypt/Crypt.php
+++ Crypt/Crypt.php
@@ -5,13 +5,14 @@ class Crypt
 	private $_key;
 	private $_iv;
 	private $_cipher;
+	private $_ivSize;
 
 	function __construct($key = '') {
 		$this->_key = $key;
 		$this->_cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-		$ivSize = mcrypt_enc_get_iv_size($this->_cipher);
-		$tmpIV = bin2hex(mcrypt_create_iv($ivSize));
-		$this->_iv = substr($tmpIV, 0, $ivSize);
+		$this->_ivSize = mcrypt_enc_get_iv_size($this->_cipher);
+		$tmpIV = bin2hex(mcrypt_create_iv($this->_ivSize));
+		$this->_iv = substr($tmpIV, 0, $this->_ivSize);
 
 	}
 
@@ -24,7 +25,9 @@ class Crypt
 	}
 
 	public function decrypt($data) {
-		list($this->_iv, $data) = explode(":", base64_decode($data));
+		$raw_data = base64_decode($data);
+		$this->_iv = substr($raw_data, 0, $this->_ivSize);
+		$data = substr($raw_data, $this->_ivSize + 1);
 
 		mcrypt_generic_init($this->_cipher, $this->_key, $this->_iv);
 		$result = mdecrypt_generic($this->_cipher, base64_decode($data));
