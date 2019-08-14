83b4929d78759a07fe090edf6814b220473d3f93
Boris <bcruchet@camelsecure.com>
2019-08-14 13:10:14 -0400

    Refactor the array notation for htmlform example code

--- examples/htmlform_example.php
+++ examples/htmlform_example.php
@@ -9,25 +9,25 @@ $f->addField('Your name', 'name', 'text', '', array('id' => 'name', 'maxlenght'=
 $f->addField('Your Email', 'email', 'text', '', array('id'=> 'email', 'maxlenght'=>'100'));
 $f->addField('Your Password', 'password', 'password', '', array('id'=> 'password', 'maxlenght'=>'64'));
 
-$values = array(
+$values = [
 	'mr' => 'Mister',
 	'ms' => 'Miss'
-);
+];
 $f->addField('Title', 'title', 'radio', $values);
 
-$values = array(
+$values = [
 	'b' => 'Banana',
 	'o' => 'Orange',
 	'a' => 'Apple'
-);
+];
 $f->addField('Fruit', 'fruits', 'checkbox', $values);
 
-$options = array(
+$options = [
 	'0' => '----',
 	'1' => 'Valdivia',
 	'2' => 'Santiago',
 	'3' => 'La Serena',
-);
+];
 
 $f->addField('Select your city', 'city', 'select', $options);
 $f->addField('Your Comment', 'description', 'textarea', 'your description here', array('cols' => 40, 'rows' => '10'));
@@ -36,4 +36,3 @@ $f->addField('', 'send', 'submit', 'Send Form', array('id'=> 'send'));
 echo "<html><body>\n";
 echo $f->render()."\n";
 echo "</body></html>\n";
-
