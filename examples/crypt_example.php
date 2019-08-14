f3b262c8ebfb4d4c74bdc09dd4b9df6464435944
Boris <bcruchet@camelsecure.com>
2019-08-14 13:02:32 -0400

    Refactot Crypt class and improved the example code

--- examples/crypt_example.php
+++ examples/crypt_example.php
@@ -8,11 +8,18 @@ $key = 'super_secret_key';
 $text = 'Hello world';
 
 $c1 = new Crypt($key);
-$encrypted_text = $c1->crypt($text);
+$cypherText = $c1->crypt($text);
+$len = strlen($cypherText);
+printf("Plain text: %s\n"
+    ."Encrypted len: %d\n"
+    ."Encrypted raw text: %s\n\n",
+    $text, $len, $cypherText);
 
-echo "encrypted text: $encrypted_text\n";
+$c2 = new Crypt($key, false);
+$cypherText = $c2->crypt($text);
+$len = strlen($cypherText);
 
-$c2 = new Crypt($key);
-$plain_text = $c2->decrypt($encrypted_text);
-
-echo "plain text: $plain_text\n";
+printf("Plain text: %s\n"
+    ."Encrypted len: %d\n"
+    ."Encrypted raw text: %s\n\n",
+    $text, $len, $cypherText);
--- src/Crypt.php
+++ src/Crypt.php
@@ -9,30 +9,59 @@ class Crypt
     private $ivLen;
     private $tag;
     private $cipherAlg;
+    private $encoded  = true;
+    private $option = 0;
 
-    function __construct($key = '') {
+    public function __construct($key, $encoded = true)
+    {
         $this->key = $key;
         $this->cipherAlg = 'aes-128-cbc';
+
+        if (!$encoded) {
+            $this->encoded = $encoded;
+            $this->option = OPENSSL_RAW_DATA;
+        }
 
         if (!in_array($this->cipherAlg, openssl_get_cipher_methods())) {
             throw new Exception('Unknown cipher algorithm');
         }
+
         $this->ivLen = openssl_cipher_iv_length($this->cipherAlg);
         $this->iv = openssl_random_pseudo_bytes($this->ivLen);
     }
 
-    public function crypt($data) {
-        $cipherData = openssl_encrypt($data, $this->cipherAlg, $this->key, $options=0, $this->iv);
-        return base64_encode($this->iv.":".$cipherData);
+    public function crypt($data)
+    {
+        $cipherData = openssl_encrypt(
+            $data,
+            $this->cipherAlg,
+            $this->key,
+            $this->option,
+            $this->iv
+        );
+
+        if ($this->encoded) {
+            return base64_encode($this->iv.$cipherData);
+        }
+        return $this->iv.$cipherData;
     }
 
-    public function decrypt($data) {
-        $raw_data = base64_decode($data);
-        $this->iv = substr($raw_data, 0, $this->ivLen);
-        $data = substr($raw_data, $this->ivLen + 1);
+    public function decrypt($data)
+    {
+        if ($this->encoded) {
+            $data = base64_decode($data);
+        }
 
-        $clearData = openssl_decrypt($data, $this->cipherAlg, $this->key, $options=0, $this->iv);
+        $this->iv = substr($data, 0, $this->ivLen);
+        $data = substr($data, $this->ivLen);
+
+        $clearData = openssl_decrypt(
+            $data,
+            $this->cipherAlg,
+            $this->key,
+            $this->option,
+            $this->iv
+        );
         return $clearData;
     }
 }
-
